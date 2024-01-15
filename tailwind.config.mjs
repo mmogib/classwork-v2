1a2dbec60b168e18d647956c4fb84f5ec1d4127d
Mohammed Alshahrani <mmogib@gmail.com>
2024-01-15 10:32:44 +0300

    updated cousrs names

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    colors: {
+      "kfupm-green": "#0C7E40",
+      "kfupm-gold": "#D1B755",
+      "kfupm-black": "#E0E2DE",
+    },
+  },
+  plugins: [],
+};
