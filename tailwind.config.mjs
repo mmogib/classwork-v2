786560e06cc30de8dc2e0448a3c158af41865ba3
Mohammed Alshahrani <mmogib@gmail.com>
2024-01-15 12:12:27 +0300

    fix: tailwind colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,11 +2,12 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
-    colors: {
-      "kfupm-green": "#0C7E40",
-      "kfupm-gold": "#D1B755",
-      "kfupm-black": "#E0E2DE",
+    extend: {
+      colors: {
+        "kfupm-green": "#0C7E40",
+        "kfupm-gold": "#D1B755",
+        "kfupm-black": "#E0E2DE",
+      },
     },
   },
   plugins: [],
